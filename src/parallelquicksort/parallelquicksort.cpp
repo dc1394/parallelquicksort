413d7b0782cbdc65fd5919c36f347ce5534cab5f
dc1394 <hriver2014-bitbucket@memoad.jp>
2021-01-01 15:28:02 +0900

    Intel® oneAPI DPC++/C++ Compilerに対応

--- src/parallelquicksort/parallelquicksort.cpp
+++ src/parallelquicksort/parallelquicksort.cpp
@@ -21,6 +21,14 @@
 #include <utility>                  // for std::pair
 #include <vector>                   // for std::vector
 
+#if defined(_MSC_VER) && defined(__llvm__)
+    #include <string_view>          // for std::string_view_literals
+    #include <system_error>         // for std::system_error 
+
+    #define WIN32_LEAN_AND_MEAN
+    #include <Windows.h>            // for GetLastError, MultiByteToWideChar
+#endif
+
 #ifndef _MSC_VER
     #include <parallel/algorithm>   // for __gnu_parallel::sort
 #else
@@ -62,6 +70,12 @@ namespace {
 
     //! A global variable (constant expression).
     /*!
+        計測する回数
+    */
+    static auto constexpr CPPTHREADRECMAX = 7;
+
+    //! A global variable (constant expression).
+    /*!
         ソートする配列の要素数の最初の数
     */
     static auto constexpr N = 500;
@@ -92,11 +106,21 @@ namespace {
         引数で与えられたstd::functionの実行時間をファイルに出力する
         \param checktype パフォーマンスをチェックする際の対象配列の種類
         \param func 実行するstd::function
-        \param n 配列のサイス
+        \param n 配列のサイズ
         \param ofs 出力用のファイルストリーム
         \return funcの実行結果のstd::vector
     */
     std::vector<std::int32_t> elapsed_time(Checktype checktype, std::function<void(std::vector<std::int32_t> &)> const & func, std::int32_t n, std::ofstream & ofs);
+
+#if defined(_MSC_VER) && defined(__llvm__)
+    //! A function.
+    /*!
+        UTF-8エンコーディングの文字列をShift-JISエンコーディングの文字列に変換する
+        \param u8str UTF-8エンコーディングの文字列
+        \return Shift-JISエンコーディングの文字列
+    */
+    std::string myutf8tosjis(std::string_view const & u8str);
+#endif
 
     template < class RandomIter >
     //! A template function.
@@ -324,7 +348,7 @@ namespace {
         // 部分ソートが小さくなりすぎるとシリアル実行のほうが効率が良くなるため
         // 部分ソートの要素数が閾値以上の時だけ再帰させる
         // かつ、現在の再帰の深さが物理コア数以下のときだけ再帰させる
-        if (num >= THRESHOLD && reci <= NUMPHYSICALCORE) {
+        if (num >= THRESHOLD && reci <= CPPTHREADRECMAX) {
             // 交点まで左右から入れ替えして交点を探す
             auto const middle = std::partition(first + 1, last, [first](auto n) { return n < *first; });
 
@@ -363,6 +387,222 @@ namespace {
         quick_sort_thread(first, last, 0);
     }
 
+#ifdef DEBUG_CHECK_RECUL
+    //! A function.
+    /*!
+        再帰数の上限でパフォーマンスがどう変化するかチェックする
+        \param ofs 出力用のファイルストリーム
+    */
+    void check_performance_recul(std::ofstream & ofs);
+
+    //! A function.
+    /*!
+        引数で与えられたstd::functionの実行時間をファイルに出力する
+        \param func 実行するstd::function
+        \param n 配列のサイズ
+        \param ofs 出力用のファイルストリーム
+    */
+    void elapsed_time_recul(std::function<void(std::vector<std::int32_t> &)> const & func, std::int32_t n, std::ofstream & ofs);
+
+#if _OPENMP >= 200805
+    template < class RandomIter >
+    //! A template function.
+    /*!
+        指定された範囲の要素をクイックソートでソートする（OpenMPで並列化）
+        \param first 範囲の下限
+        \param last 範囲の上限
+        \param reci 現在の再帰の深さ
+        \param recul 再帰数の上限
+    */
+    void quick_sort_openmp_recul(RandomIter first, RandomIter last, std::int32_t reci, std::int32_t recul)
+    {
+        // 部分ソートの要素数
+        auto const num = std::distance(first, last);
+
+        if (num <= 1) {
+            // 部分ソートの要素数が1個以下なら何もすることはない
+            return;
+        }
+
+        // 再帰の深さ + 1
+        reci++;
+
+        // 部分ソートが小さくなりすぎるとシリアル実行のほうが効率が良くなるため
+        // 部分ソートの要素数が閾値以上の時だけ再帰させる
+        // かつ、現在の再帰の深さが物理コア数以下のときだけ再帰させる
+        if (num >= THRESHOLD && reci <= recul) {
+            // 交点まで左右から入れ替えして交点を探す
+            auto const middle = std::partition(first + 1, last, [first](auto n) { return n < *first; });
+
+            // 交点 - 1の位置
+            auto const mid = middle - 1;
+
+            // 交点を移動
+            std::iter_swap(first, mid);
+
+            // 次の関数をタスクとして実行
+#pragma omp task
+            // 下部をソート
+            quick_sort_openmp_recul(first, mid, reci, recul);
+
+            // 次の関数をタスクとして実行
+#pragma omp task
+            // 上部をソート
+            quick_sort_openmp_recul(middle, last, reci, recul);
+
+            // 二つのタスクの終了を待機
+#pragma omp taskwait
+        }
+        else {
+            // 再帰なしのクイックソートの関数を呼び出す
+            quick_sort(first, last);
+        }
+    }
+
+    template < class RandomIter >
+    //! A template function.
+    /*!
+        指定された範囲の要素をクイックソートでソートする（OpenMPで並列化）
+        \param first 範囲の下限
+        \param last 範囲の上限
+        \param recul 再帰数の上限
+    */
+    inline void quick_sort_openmp_recul(RandomIter first, RandomIter last, std::int32_t recul)
+    {
+#pragma omp parallel    // OpenMP並列領域の始まり
+#pragma omp single      // task句はsingle領域で実行
+        // 再帰ありの並列クイックソートを呼び出す
+        quick_sort_openmp_recul(first, last, 0, recul);
+    }
+#endif
+
+    template < class RandomIter >
+    //! A template function.
+    /*!
+        指定された範囲の要素をクイックソートでソートする（TBBで並列化）
+        \param first 範囲の下限
+        \param last 範囲の上限
+        \param reci 現在の再帰の深さ
+        \param recul 再帰数の上限
+    */
+    void quick_sort_tbb_recul(RandomIter first, RandomIter last, std::int32_t reci, std::int32_t recul)
+    {
+        // 部分ソートの要素数
+        auto const num = std::distance(first, last);
+
+        if (num <= 1) {
+            // 部分ソートの要素数が1個以下なら何もすることはない
+            return;
+        }
+
+        // 再帰の深さ + 1
+        reci++;
+
+        // 部分ソートが小さくなりすぎるとシリアル実行のほうが効率が良くなるため
+        // 部分ソートの要素数が閾値以上の時だけ再帰させる
+        // かつ、現在の再帰の深さが物理コア数以下のときだけ再帰させる
+        if (num >= THRESHOLD && reci <= recul) {
+            // 交点まで左右から入れ替えして交点を探す
+            auto const middle = std::partition(first + 1, last, [first](auto n) { return n < *first; });
+
+            // 交点 - 1の位置
+            auto const mid = middle - 1;
+
+            // 交点を移動
+            std::iter_swap(first, mid);
+
+            // 二つのラムダ式を別スレッドで実行
+            tbb::parallel_invoke(
+                // 下部をソート
+                [first, mid, reci, recul]() { quick_sort_tbb_recul(first, mid, reci, recul); },
+                // 上部をソート
+                [middle, last, reci, recul]() { quick_sort_tbb_recul(middle, last, reci, recul); });
+        }
+        else {
+            // 再帰なしのクイックソートの関数を呼び出す
+            quick_sort(first, last);
+        }
+    }
+
+    template < class RandomIter >
+    //! A template function.
+    /*!
+        指定された範囲の要素をクイックソートでソートする（TBBで並列化）
+        \param first 範囲の下限
+        \param last 範囲の上限
+        \param recul 再帰数の上限
+    */
+    inline void quick_sort_tbb_recul(RandomIter first, RandomIter last, std::int32_t recul)
+    {
+        // 再帰ありの並列クイックソートの関数を呼び出す
+        quick_sort_tbb_recul(first, last, 0, recul);
+    }
+
+    template < class RandomIter >
+    //! A template function.
+    /*!
+        指定された範囲の要素をクイックソートでソートする（std::threadで並列化）
+        \param first 範囲の下限
+        \param last 範囲の上限
+        \param reci 現在の再帰の深さ
+        \param recul 再帰数の上限
+    */
+    void quick_sort_thread_recul(RandomIter first, RandomIter last, std::int32_t reci, std::int32_t recul)
+    {
+        // 部分ソートの要素数
+        auto const num = std::distance(first, last);
+
+        if (num <= 1) {
+            // 部分ソートの要素数が1個以下なら何もすることはない
+            return;
+        }
+
+        // 再帰の深さ + 1
+        reci++;
+
+        // 部分ソートが小さくなりすぎるとシリアル実行のほうが効率が良くなるため
+        // 部分ソートの要素数が閾値以上の時だけ再帰させる
+        // かつ、現在の再帰の深さが物理コア数以下のときだけ再帰させる
+        if (num >= THRESHOLD && reci <= recul) {
+            // 交点まで左右から入れ替えして交点を探す
+            auto const middle = std::partition(first + 1, last, [first](auto n) { return n < *first; });
+
+            // 交点 - 1の位置
+            auto const mid = middle - 1;
+
+            // 交点を移動
+            std::iter_swap(first, mid);
+
+            // 下部をソート（別スレッドで実行）
+            auto th1 = std::thread([first, mid, reci, recul]() { quick_sort_thread_recul(first, mid, reci, recul); });
+
+            // 上部をソート（別スレッドで実行）
+            auto th2 = std::thread([middle, last, reci, recul]() { quick_sort_thread_recul(middle, last, reci, recul); });
+
+            // 二つのスレッドの終了を待機
+            th1.join();
+            th2.join();
+        }
+        else {
+            // 再帰なしのクイックソートの関数を呼び出す
+            quick_sort(first, last);
+        }
+    }
+
+    template < class RandomIter >
+    //! A template function.
+    /*!
+        指定された範囲の要素をクイックソートでソートする（std::threadで並列化）
+        \param first 範囲の下限
+        \param last 範囲の上限
+    */
+    inline void quick_sort_thread_recul(RandomIter first, RandomIter last, std::int32_t recul)
+    {
+        // 再帰ありの並列クイックソートの関数を呼び出す
+        quick_sort_thread_recul(first, last, 0, recul);
+    }
+#endif
+
 #ifdef DEBUG
     //! A template function.
     /*!
@@ -377,9 +617,44 @@ namespace {
 
 int main()
 {
+#if defined(_MSC_VER) && defined(__llvm__)
+    using namespace std::string_view_literals;
+
+    std::cout << myutf8tosjis(u8R"(物理コア数: )"sv) << boost::thread::physical_concurrency();
+    std::cout << myutf8tosjis(u8R"(, 論理コア数: )"sv) << boost::thread::hardware_concurrency() << std::endl;
+
+#ifdef DEBUG_CHECK_RECUL
+    std::ofstream ofsrec(myutf8tosjis(u8R"(完全にシャッフルされたデータ_再帰数チェック.csv)"sv));
+    check_performance_recul(ofsrec);
+#else
+    std::ofstream ofsrandom(myutf8tosjis(u8R"(完全にシャッフルされたデータ.csv)"sv));
+    std::ofstream ofssort(myutf8tosjis(u8R"(あらかじめソートされたデータ.csv)"sv));
+    std::ofstream ofsquartersort(myutf8tosjis(u8R"(最初の1_4だけソートされたデータ.csv)"sv));
+
+    std::cout << myutf8tosjis(u8R"(完全にシャッフルされたデータを計測中...)"sv) << '\n';
+    if (!check_performance(Checktype::RANDOM, ofsrandom)) {
+        return -1;
+    }
+
+    std::cout << '\n' << myutf8tosjis(u8R"(あらかじめソートされたデータを計測中...)"sv) << '\n';
+    if (!check_performance(Checktype::SORT, ofssort)) {
+        return -1;
+    }
+
+    std::cout << '\n' << myutf8tosjis(u8R"(最初の1/4だけソートされたデータを計測中...)"sv) << '\n';
+    if (!check_performance(Checktype::QUARTERSORT, ofsquartersort)) {
+        return -1;
+    }
+#endif
+
+#else
     std::cout << "物理コア数: " << boost::thread::physical_concurrency();
     std::cout << ", 論理コア数: " << boost::thread::hardware_concurrency() << std::endl;
 
+#ifdef DEBUG_CHECK_RECUL
+    std::ofstream ofsrec("完全にシャッフルされたデータ_再帰数チェック.csv");
+    check_performance_recul(ofsrec);
+#else
     std::ofstream ofsrandom("完全にシャッフルされたデータ.csv");
     std::ofstream ofssort("あらかじめソートされたデータ.csv");
     std::ofstream ofsquartersort("最初の1_4だけソートされたデータ.csv");
@@ -398,6 +673,8 @@ int main()
     if (!check_performance(Checktype::QUARTERSORT, ofsquartersort)) {
         return -1;
     }
+#endif
+#endif
 
     return 0;
 }
@@ -410,9 +687,11 @@ namespace {
         ofs.write(reinterpret_cast<char const *>(bom.data()), sizeof(bom));
 #endif
 
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && _OPENMP < 200805
         ofs << "配列の要素数,std::sort,クイックソート,std::thread,TBB,concurrency::parallel_sort,concurrency::parallel_buffered_sort,tbb::parallel_sort,std::sort (MSVC内蔵のParallelism TS),std::sort (Parallel STLのParallelism TS)\n";
-#else
+#elif defined(_MSC_VER)
+        ofs << "配列の要素数,std::sort,クイックソート,std::thread,TBB,OpenMP,concurrency::parallel_sort,concurrency::parallel_buffered_sort,tbb::parallel_sort,std::sort (MSVC内蔵のParallelism TS),std::sort (Parallel STLのParallelism TS)\n";
+#elif
         ofs << u8"配列の要素数,std::sort,クイックソート,std::thread,OpenMP,TBB,__gnu_parallel::sort,tbb::parallel_sort,std::sort (Parallelism TS),std::sort (Parallel STLのParallelism TS)\n";
 #endif
         
@@ -421,8 +700,13 @@ namespace {
         auto n = N;
         for (auto i = 0; i < 6; i++) {
             for (auto j = 0; j < 2; j++) {
-                std::cout << n << "個を計測中...\n";
+#if defined(_MSC_VER) && defined(__llvm__)
+                using namespace std::string_view_literals;
 
+                std::cout << n << myutf8tosjis(u8R"(個を計測中...)"sv) << '\n';
+#else
+                std::cout << n << "個を計測中...\n";
+#endif
                 std::vector<std::int32_t> vec(n);
                 std::iota(vec.begin(), vec.end(), 1);
 
@@ -443,9 +727,9 @@ namespace {
 #ifndef _MSC_VER
                     vecar[5] = elapsed_time(checktype, [](auto && vec) { __gnu_parallel::sort(vec.begin(), vec.end()); }, n, ofs);
 #else
-                    vecar[5] = elapsed_time(checktype, [](auto&& vec) { concurrency::parallel_sort(vec.begin(), vec.end()); }, n, ofs);
+                    vecar[5] = elapsed_time(checktype, [](auto && vec) { concurrency::parallel_sort(vec.begin(), vec.end()); }, n, ofs);
 
-                    vecar[6] = elapsed_time(checktype, [](auto&& vec) { concurrency::parallel_buffered_sort(vec.begin(), vec.end()); }, n, ofs);
+                    vecar[6] = elapsed_time(checktype, [](auto && vec) { concurrency::parallel_buffered_sort(vec.begin(), vec.end()); }, n, ofs);
 #endif
 
 
@@ -468,30 +752,24 @@ namespace {
                 ofs << std::endl;
 
 #ifdef DEBUG
-                for (auto i = 0U; i < vecar.size(); i++) {
+                for (auto k = 0U; k < vecar.size(); k++) {
 #if _OPENMP < 200805
-                    if (i == 3) {
+                    if (k == 3) {
                         continue;
                     }
 #endif
 
-#if !defined(__INTEL_COMPILER) && (__GNUC__ < 5 || __GNUC__ > 8)
-                    if (i == 5) {
-                        continue;
-                    }
-#endif
-
-                    if (!i || i == 6 || i == 7 || i == 8) {
+                    if (!k || k == 6 || k == 7 || k == 8) {
                         continue;
                     }
 
-                    if (static_cast<std::int32_t>(vecar[i].size()) != n) {
+                    if (static_cast<std::int32_t>(vecar[k].size()) != n) {
                         issuccess = false;
                         continue;
                     }
 
-                    if (!vec_check(vec, vecar[i])) {
-                        std::cerr << "Error! vecar[" << i << ']' << std::endl;
+                    if (!vec_check(vec, vecar[k])) {
+                        std::cerr << "Error! vecar[" << k << ']' << std::endl;
                         issuccess = false;
                     }
                 }
@@ -581,6 +859,123 @@ namespace {
 
         return vec;
     }
+
+#ifdef DEBUG_CHECK_RECUL
+    void check_performance_recul(std::ofstream& ofs)
+    {
+        using namespace std::string_view_literals;
+
+#if _OPENMP < 200805
+        ofs << "再帰数,std::thread,TBB\n";
+#else
+        ofs << myutf8tosjis(u8R"(再帰数,std::thread,OpenMP,TBB)"sv) << '\n';
+#endif
+
+        for (auto recul = 0U; recul <= boost::thread::hardware_concurrency(); recul++) {
+#if defined(_MSC_VER) && defined(__llvm__)
+            std::cout << myutf8tosjis(u8R"(再帰数: )"sv) << recul << myutf8tosjis(u8R"(を計測中...)"sv) << '\n';
+#else
+            std::cout << "再帰数: " << recul << "を計測中...\n";
+#endif
+
+            ofs << recul << ',';
+            elapsed_time_recul([recul](auto && vec) { quick_sort_thread_recul(vec.begin(), vec.end(), recul); }, 5000000, ofs);
+#if _OPENMP >= 200805
+            elapsed_time_recul([recul](auto && vec) { quick_sort_openmp_recul(vec.begin(), vec.end(), recul); }, 5000000, ofs);
+#endif
+            elapsed_time_recul([recul](auto && vec) { quick_sort_tbb_recul(vec.begin(), vec.end(), recul); }, 5000000, ofs);
+            ofs << std::endl;
+        }
+    }
+
+    void elapsed_time_recul(std::function<void(std::vector<std::int32_t>&)> const& func, std::int32_t n, std::ofstream& ofs)
+    {
+        using namespace std::chrono;
+
+        std::vector<std::int32_t> vec(n);
+        std::unique_ptr< FILE, decltype(&std::fclose) > fp(nullptr, fclose);
+
+        auto const program_name = "makequicksortdata";
+
+        auto const filename = (boost::format("sortdata_%d_rand.dat") % n).str();
+        fp = std::unique_ptr< FILE, decltype(&std::fclose) >(std::fopen(filename.c_str(), "rb"), std::fclose);
+        if (!fp) {
+            boost::process::child(program_name + (boost::format(" 0 %d") % n).str()).wait();
+            fp = std::unique_ptr< FILE, decltype(&std::fclose) >(std::fopen(filename.c_str(), "rb"), std::fclose);
+        }
+
+        auto const readsize = vec.size();
+        if (readsize != std::fread(vec.data(), sizeof(std::int32_t), readsize, fp.get())) {
+            throw std::runtime_error("std::freadに失敗");
+        }
+
+        auto elapsed_time = 0.0;
+
+        for (auto i = 1; i <= CHECKLOOP; i++) {
+            auto const beg = high_resolution_clock::now();
+            func(vec);
+            auto const end = high_resolution_clock::now();
+
+            elapsed_time += (duration_cast<duration<double>>(end - beg)).count();
+
+            if (i != CHECKLOOP) {
+                std::rewind(fp.get());
+                if (readsize != std::fread(vec.data(), sizeof(std::int32_t), readsize, fp.get())) {
+                    throw std::runtime_error("std::freadに失敗");
+                }
+            }
+        }
+
+        ofs << boost::format("%.10f") % (elapsed_time / static_cast<double>(CHECKLOOP)) << ',';
+    }
+#endif
+
+#if defined(_MSC_VER) && defined(__llvm__)
+    std::string myutf8tosjis(std::string_view const & u8str)
+    {
+        // UTF-8 -> UTF-16
+        auto length = ::MultiByteToWideChar(
+            CP_UTF8,
+            0,
+            reinterpret_cast<const char*>(u8str.data()),
+            static_cast<int>(u8str.length()),
+            nullptr,
+            0);
+        if (!length) {
+            throw std::system_error(std::error_code(GetLastError(), std::system_category()));
+        }
+
+        std::wstring temp(length, '\0');
+
+        auto res = ::MultiByteToWideChar(
+            CP_UTF8,
+            0,
+            reinterpret_cast<const char*>(u8str.data()),
+            static_cast<int>(u8str.length()),
+            temp.data(), temp.length());
+        if (!res) {
+            throw std::system_error(std::error_code(GetLastError(), std::system_category()));
+        }
+
+        // UTF-16 -> Shift-JIS
+        length = ::WideCharToMultiByte(CP_ACP, 0,
+            temp.data(), static_cast<int>(temp.length()),
+            nullptr, 0,
+            nullptr, nullptr);
+
+        std::string result(length, '\0');
+
+        res = ::WideCharToMultiByte(CP_ACP, 0,
+            temp.data(), static_cast<int>(temp.length()),
+            result.data(), static_cast<int>(result.length()),
+            nullptr, nullptr);
+        if (!res) {
+            throw std::system_error(std::error_code(GetLastError(), std::system_category()));
+        }
+
+        return result;
+    }
+#endif
 
 #ifdef DEBUG
     bool vec_check(std::vector<std::int32_t> const & v1, std::vector<std::int32_t> const & v2)
